172a3461e99f693fc33436d392d234f528c82606
vswamymengani <snowdropin2017@gmail.com>
2026-08-21 19:43:48 +0530

    new changes for functions

--- functiondemo.py
+++ functiondemo.py
@@ -1,5 +1,22 @@
+#Simple Function, No parameters and No returns
 def welcome_banner():
     print("--------------------------")
     print("  Welcome to Apex Banking  ")
     print("--------------------------")
+###Parameterized Function (With Inputs, No Return)
+###Greets a specific customer using their profile details
+def customer_greeting(customer_name,Account_type):
+    print(f"Hello,{customer_name}! managing your {Account_type} Account")
 welcome_banner()
+customer_greeting("Venkat","Saving")
+print("                           ")
+##Function with a Return Value (With Inputs and Output)
+##Calculates total balance from a list of transactions
+def bank_transactions(transactions):
+    total=sum(transactions)
+    return total
+my_transactions=[120,800,-100,-99,500]
+current_balance=bank_transactions(my_transactions)
+print(f"my current balance is:  ${current_balance}")
+print("                           ")
+print("--------------------------")
